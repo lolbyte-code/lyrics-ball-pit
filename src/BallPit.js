017fbe32ce9bb29192caac01cfab5307651df781
LolByte <crxlolbyte@gmail.com>
2021-11-21 10:26:39 -0800

    Use letters from lyrics

--- src/BallPit.js
+++ src/BallPit.js
@@ -1,65 +1,59 @@
 import { useEffect, useRef } from 'react'
 import { Engine, Render, Runner, Composites, MouseConstraint, Mouse, Composite, Bodies } from 'matter-js'
+import { getLetters } from './Lyrics'
 
 const BallPit = () => {
     const scene = useRef()
 
     useEffect(() => {
         // create engine
-        var engine = Engine.create(),
+        const engine = Engine.create(),
             world = engine.world;
-
-        // create renderer
-        var render = Render.create({
+        const render = Render.create({
             element: document.body,
             engine: engine,
             options: {
                 width: 800,
                 height: 600,
                 wireframes: false,
+                background: 'white',
             }
         });
-
         Render.run(render);
-
-        // create runner
-        var runner = Runner.create();
+        const runner = Runner.create();
         Runner.run(runner, engine);
 
-        // add bodies
-        var stack = Composites.stack(20, 20, 100, 5, 0, 0, function (x, y) {
-
-            return Bodies.circle(x, y, 15, {
-                render: {
-                    // fillStyle: 'white',
-                    // strokeStyle: 'black',
-                    // lineWidth: 1,
-                    // text: {
-                    //     content: letters[Math.floor(Math.random()*letters.length)],
-                    //     color: "white",
-                    //     size: 50,
-                    // },
-                    sprite: {
-                        texture: 'letters/b.png',
-                        xScale: .055,
-                        yScale: .055,
-                    }
+        Object.entries(getLetters()).forEach(group => {
+            const letter = group[0]
+            const count = group[1]
+            const stack = Composites.stack(400, 0, 1, count, 0, 0, function (x, y) {
+                var path = `letters/${letter}.png`
+                if (letter.match(/[A-Z]/g)) {
+                    path = `letters/uppercase/${letter.toLowerCase()}.png`
                 }
-            })
-        });
-
-        Composite.add(world, stack);
+                return Bodies.circle(x, y, 20, {
+                    render: {
+                        sprite: {
+                            texture: path,
+                            xScale: .3,
+                            yScale: .3,
+                        }
+                    }
+                })
+            });
+            Composite.add(world, stack);
+        })
 
         Composite.add(world, [
             // walls
-            Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-            Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-            Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-            Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+            Bodies.rectangle(400, 0, 800, 150, { isStatic: true }),
+            Bodies.rectangle(400, 950, 50, 50, { isStatic: false }),
+            Bodies.rectangle(400, 1000, 800, 150, { isStatic: true }),
+            Bodies.rectangle(750, 500, 120, 1600, { isStatic: true }),
+            Bodies.rectangle(50, 500, 120, 1600, { isStatic: true })
         ]);
 
-        // add mouse control
-        var mouse = Mouse.create(render.canvas),
+        const mouse = Mouse.create(render.canvas),
             mouseConstraint = MouseConstraint.create(engine, {
                 mouse: mouse,
                 constraint: {
@@ -69,19 +63,13 @@ const BallPit = () => {
                     }
                 }
             });
-
         Composite.add(world, mouseConstraint);
-
-        // keep the mouse in sync with rendering
         render.mouse = mouse;
-
-        // fit the render viewport to the scene
         Render.lookAt(render, {
             min: { x: 0, y: 0 },
-            max: { x: 800, y: 600 }
+            max: { x: 800, y: 1000 }
         });
 
-        // context for MatterTools.Demo
         return {
             engine: engine,
             runner: runner,
---
+++ src/Lyrics.js
@@ -0,0 +1,53 @@
+const lyrics = `
+We're all looking for someone, that perfect girl
+Who can take our hand and help us see the world
+But I'm a little unusual, because you see
+I seek a different type of thrill for my romantic fantasy
+And I must say
+I'm looking your way
+And you seem to like me, baby
+And you give me all the signs
+There's just one activity on my mind
+Do you wanna do nothing with me?
+Do you wanna do nothing with me?
+Do you wanna do nothing with me?
+Do you wanna do nothing with me?
+Do you wanna do something with me tonight?
+I'll put on my finest sweatpants and I'll order you pad thai
+We'd be livin' our dreams, as our love grows
+What an intimate occasion, you and me and HBO
+'Til morning light, so wrong yet so right
+So say you want it, darling
+'Cause you know I want it too
+And there's just one thing that I wanna do
+Do you wanna do nothing with me?
+Do you wanna do nothing with me?
+Do you wanna do nothing with me?
+Do you wanna do nothing with me?
+Oh, oh, let's talk it through
+In case you're confused
+'Cause I know I can seem lazy
+From these things I like to do
+But they're only worth it when I'm with you
+Yeah, it's true, so do you
+Wanna do nothing with me?
+Oh, do you wanna do nothing with me?
+Do you wanna do nothing with me?
+Do you wanna do nothing with me?
+`
+
+export const getLetters = () => {
+    const letters = lyrics.split("").filter(letter => letter.match(/[A-z]/g))
+    return groupBy(letters)
+}
+
+const groupBy = (arr) => {
+    return arr.reduce((acc, obj) => {
+       const key = obj
+       if (!acc[key]) {
+          acc[key] = 0
+       }
+       acc[key]++
+       return acc
+    }, {})
+}
